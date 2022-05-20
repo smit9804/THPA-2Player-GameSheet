56160bc463d1b2369f0132d02ef0a379945de125
Chase A Smith <74937395+smit9804@users.noreply.github.com>
2022-05-19 23:07:08 -0500

    First Commit

---
+++ scoreobject.js
@@ -0,0 +1,38 @@
+const p1ScoreObject = {
+    shoeNumber: 0,
+    scoreType: [],
+    deadRingers: 0,
+    liveRingers: 0,
+    closeShoe: 0,
+    score: 0,
+    scoreHistory: function() {
+        return this.scoreType;
+    }
+}
+const p2ScoreObject = {
+    shoeNumber: 0,
+    scoreType: [],
+    deadRingers: 0,
+    liveRingers: 0,
+    closeShoe: 0,
+    score: 0,
+    scoreHistory: function() {
+        return this.scoreType;
+    }
+}
+
+const rowObject = {
+    deadp1: "-",
+    livep1: "-",
+    ringerp1: 0,
+    closep1: 0,
+    score: 0,
+    shoes: 0,
+    deadp2: "-",
+    livep2: "-",
+    ringerp2: 0,
+    closep2: 0,
+    score: 0 
+}
+
+
